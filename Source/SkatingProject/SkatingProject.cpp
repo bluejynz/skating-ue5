fb668cde8e017da9d81e8eccb206b6e7110ec5a7
bluejynz <dany_falk@hotmai.com>
2024-06-29 17:29:06 -0300

    Initial commit

---
+++ Source/SkatingProject/SkatingProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SkatingProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SkatingProject, "SkatingProject" );
+ 
